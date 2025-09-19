ffc92cb2053a1337ae91113f0aadbcb6fd43ebce
Nidhish Senthilkumar <nidhishsenthilkumar@gmail.com>
2025-09-18 21:58:43 -0500

    Update LLMAIIntegration.py

--- LLMAIIntegration.py
+++ LLMAIIntegration.py
@@ -21,6 +21,7 @@ def speak_text(text):
     engine.runAndWait()
 
 # Example usage
+user_input = input("Ask something(make sure it is very clear): ")
 response = query_ollama("hi")
 print(response)
 speak_text(response)
